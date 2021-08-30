42e68be6774fc760dda28cf56ff2dc5f0d69d8e7
EkinoksAhmet <80361129+EkinoksAhmet@users.noreply.github.com>
2021-08-30 15:22:16 +0300

    Eşitleme işlemi

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 window.addEventListener("contextmenu",function(ozel_sag_tiklama_sag_click_acilan_menu_islem){
-    ozel_sag_tiklama_sag_click_acilan_menu_islem.preventDefault();
-    const ozel_sag_tiklama_sag_click_acilan_menu = document.getElementById("ozel_sag_tiklama_sag_click_acilan_menu");
-    ozel_sag_tiklama_sag_click_acilan_menu.style.top = ozel_sag_tiklama_sag_click_acilan_menu_islem.offsetY + "px";
-    ozel_sag_tiklama_sag_click_acilan_menu.style.left = ozel_sag_tiklama_sag_click_acilan_menu_islem.offsetX + "px";
-    ozel_sag_tiklama_sag_click_acilan_menu.classList.add("active");
-  });
-  
-  window.addEventListener("click",function(){
-    document.getElementById("ozel_sag_tiklama_sag_click_acilan_menu").classList.remove("active");
-  });
+  ozel_sag_tiklama_sag_click_acilan_menu_islem.preventDefault();
+  const ozel_sag_tiklama_sag_click_acilan_menu = document.getElementById("ozel_sag_tiklama_sag_click_acilan_menu");
+  ozel_sag_tiklama_sag_click_acilan_menu.style.top = ozel_sag_tiklama_sag_click_acilan_menu_islem.offsetY + "px";
+  ozel_sag_tiklama_sag_click_acilan_menu.style.left = ozel_sag_tiklama_sag_click_acilan_menu_islem.offsetX + "px";
+  ozel_sag_tiklama_sag_click_acilan_menu.classList.add("active");
+});
+
+window.addEventListener("click",function(){
+  document.getElementById("ozel_sag_tiklama_sag_click_acilan_menu").classList.remove("active");
+});
